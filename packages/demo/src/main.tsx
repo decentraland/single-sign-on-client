868e357abf103d7ad1e877f541dbef929c331a4e
Nando <24811313+fzavalia@users.noreply.github.com>
2023-10-20 12:23:50 -0300

    fix: Add comment

--- packages/demo/src/main.tsx
+++ packages/demo/src/main.tsx
@@ -4,6 +4,9 @@ import * as SingleSignOn from "@dcl/single-sign-on-client";
 import App from "./App.tsx";
 
 const ssoSrc = "https://localhost:3001/";
+
+// Required options to work with localhost.
+// Change to an empty object when using https://id.decentraland.{env}
 const ssoOpt = { require_tld: false, protocols: undefined };
 
 SingleSignOn.init(ssoSrc, ssoOpt);
