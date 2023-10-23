8c3943fa61be5ac00d32b50113921e2eb4a72f6f
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2023-10-23 10:35:55 -0300

    fix: Default options (#4)

--- packages/demo/src/main.tsx
+++ packages/demo/src/main.tsx
@@ -3,8 +3,13 @@ import ReactDOM from "react-dom/client";
 import * as SingleSignOn from "@dcl/single-sign-on-client";
 import App from "./App.tsx";
 
-// SingleSignOn.init("https://id.decentraland.org");
-SingleSignOn.init("http://localhost:3001");
+const ssoSrc = "http://localhost:3001/";
+
+// Required options to work with localhost.
+// Change to an empty object when using https://id.decentraland.{env}
+const ssoOpt = { require_tld: false, protocols: undefined };
+
+SingleSignOn.init(ssoSrc, ssoOpt);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
