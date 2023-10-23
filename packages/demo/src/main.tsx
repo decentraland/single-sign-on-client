ad66d1d00792198f283ff81e2dd93f4be9f5d680
Nando <24811313+fzavalia@users.noreply.github.com>
2023-10-23 10:32:56 -0300

    fix: Update protocol

--- packages/demo/src/main.tsx
+++ packages/demo/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import * as SingleSignOn from "@dcl/single-sign-on-client";
 import App from "./App.tsx";
 
-const ssoSrc = "https://localhost:3001/";
+const ssoSrc = "http://localhost:3001/";
 
 // Required options to work with localhost.
 // Change to an empty object when using https://id.decentraland.{env}
